6b0a64e7c25db115f418be92b350192ebd3b9055
Kionte Baker <kionte.baker@blackburn.edu>
2021-03-21 22:28:08 -0500

    token testing

---
+++ ServerSideApplication/src/main/java/com/servers/application/json_objects/Token.java
@@ -0,0 +1,22 @@
+package com.servers.application.json_objects;
+
+public class Token {
+    private String token;
+    private String ip;
+
+    public String getToken(){
+        return token;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+}
