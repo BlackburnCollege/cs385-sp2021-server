1df1e67334627627fd735ea9ffa1ff737cc2f896
Kionte Baker <kionte.baker@blackburn.edu>
2021-04-13 00:19:50 -0500

    this

--- ServerSideApplication/src/main/java/com/servers/application/json_objects/Token.java
+++ ServerSideApplication/src/main/java/com/servers/application/json_objects/Token.java
@@ -1,20 +1,18 @@
 package com.servers.application.json_objects;
 
 public class Token {
-    private String token;
+    private String value;
     private String ip;
 
     public String getToken(){
-        return token;
+        return value;
     }
 
     public String getIp() {
         return ip;
     }
 
-    public void setToken(String token) {
-        this.token = token;
-    }
+    public void setToken(String token) { this.value = token; }
 
     public void setIp(String ip) {
         this.ip = ip;
